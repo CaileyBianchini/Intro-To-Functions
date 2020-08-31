cf1427b3049b98596c59f53e2743b89fb0928b85
Cailey Bianchini <cailey.m.bianchini@gmail.com>
2020-08-31 16:38:42 -0500

    what the frick is going on??????
    Completly disorganized so I will have to fix it at home

--- Game.cs
+++ Game.cs
@@ -6,6 +6,63 @@ namespace HelloWorld
 {
     class Game
     {
+       
+
+
+        string _playerName;
+        
+        void RequestName()
+        {
+            char input = ' ';
+            while (input != '1')
+            {
+                Console.Clear();
+                if (_playerName != "")
+                {
+                    return;
+                }
+
+                Console.WriteLine("Welcome to Topia! What is your name?");
+                _playerName = Console.ReadLine();
+                Console.WriteLine("Hello " + _playerName);
+                Console.WriteLine("Are you sure you want your name to be: " + _playerName);
+                Console.WriteLine("1. Yes");
+                Console.WriteLine("2. No");
+                Console.WriteLine("> ");
+                input = Console.ReadKey().KeyChar;
+                Console.WriteLine();
+                input = GetInput("Yes", "No", "");
+                if(input == '2')
+                {
+                    Console.WriteLine("Yeah that name was pretty terrible wasn't it?");
+                }
+            }
+            _gameOver = true;
+        }
+
+        char GetInput(string option1, string option2, string quiry)
+        {
+
+
+            char input = ' ';
+            while (input != '1' && input != '2')
+            {
+                Console.WriteLine(quiry);
+                Console.WriteLine("1. " + option1);
+                Console.WriteLine("2. " + option2);
+                Console.WriteLine("3. View Stats");
+
+                Console.WriteLine("> ");
+                input = Console.ReadKey().KeyChar;
+                Console.WriteLine();
+
+                if (input == '3')
+                {
+                    ViewStats();
+                }
+            }
+            return input;
+        }
 
         //private bool gameOver;
         bool _gameOver = false;
@@ -16,9 +73,10 @@ namespace HelloWorld
             while (_gameOver == false)
             {
                 Update();
+                Explore();
             }
-            End();    
-          
+            End();
+
 
         }
 
@@ -31,6 +89,46 @@ namespace HelloWorld
             Console.ReadKey();
         }
 
+        void Explore()
+        {
+            char input = ' ';
+            input = GetInput("Go Left", "Go Right", "You came to a fork in the road.");
+            if (input == '1')
+            {
+                Console.WriteLine("As you were walking you heard a snap. You turn around and see a person aprroaching you with a crossbow. " +
+                    "You take a step back and all of the sudden you are wipped in the air. You got trapped in a net!");
+                Console.WriteLine("- Y O U  D I E D -");
+                _gameOver = true;
+                
+            }
+            if (input == '2')
+            {
+                Console.WriteLine("You wonder down the road for awhile and when dawn breaks you find yourself at a village.");
+            }
+
+        }
+        
+        public void asdfghj() //didn't see name
+        {
+            RequestName();
+            ViewStats();
+        }
+
+        void ViewStats()
+        {
+            Console.WriteLine("- Menu -");
+            //invatory
+            //look at stats
+            Console.WriteLine(_playerName);
+            Console.WriteLine("Press any key to continue.");
+            Console.Write("> ");
+            Console.ReadKey();
+
+        }
+
+        
+
+      
         //Repeated until the game ends
         //used for all game logic that will repeat
         //updates images
